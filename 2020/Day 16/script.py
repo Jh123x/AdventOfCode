5819f48612cb52ef0a451cda0849c430f076d7d7
Junhua <wenjh1998@gmail.com>
2020-12-16 19:25:44 +0800

    Update part 1

--- 2020/Day 16/script.py
+++ 2020/Day 16/script.py
@@ -1,11 +1,11 @@
+from intervaltree import IntervalTree, Interval
+
 def parse(x:list):
 
     #Parse the ranges
     ranges_data = x[0].split("\n")
-    print(ranges_data)
     ranges = {}
     for line in ranges_data:
-        print(line)
         key, value = line.split(': ')
         value = value.split(" or ")
         acc = []
@@ -18,25 +18,52 @@ def parse(x:list):
     my_ticket = tuple(map(int, x[1].split("\n")[1].split(",")))
 
     #Parse the remaining tickets
-    nearbytix = tuple(map(lambda y: y.split(','), x[2].split('\n')[1:]))
+    nearbytix = tuple(map(lambda y: tuple(map(int,y.split(','))), x[2].split('\n')[1:]))
 
     #Return the values
-    return ranges, my_ticket, nearbytix
+    return ranges, my_ticket, nearbytix    
 
 
-def part1():
-    pass
+def combine_ranges(ranges:tuple):
+    ran = IntervalTree()
+    for r in ranges:
+        for start,end in r:
+            ran.add(Interval(int(start), int(end) + 1))
+    return ran
 
-def part2():
-    pass
+
+def part1(ranges:tuple, nearby_tickets:tuple):
+    combined_range = combine_ranges(tuple(ranges.values()))
+    invalid_acc = 0
+    valid = []
+    for ticket in nearby_tickets:
+        for item in ticket:
+            if(not combined_range.overlaps(item)):
+                invalid_acc += item
+                break
+        else:
+            valid.append(ticket)
+    return invalid_acc, valid
+
 
 #Read the file
 with open('data.txt') as file:
     temp = file.read().split("\n\n")
-    print(len(temp))
-    ranges, my_ticket, nearby_ticket = tuple(map(parse, temp))
+    ranges, my_ticket, nearby_tickets = parse(temp)
 
-print(ranges)
+test_input = """class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
 
-# print(part1(data))
-# print(part2(data))
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12"""
+# test_range, test_tix, test_nearby = parse(test_input.split("\n\n"))
+# print(part1(test_range, test_nearby))
+print(part1(ranges, nearby_tickets)[0])
+print(part2(ranges, my_ticket, nearby_tickets))
