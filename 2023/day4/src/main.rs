e583e2c7ebb35eec7b4967de3bfb16a511354fa8
Wen Junhua <wenjh1998@gmail.com>
2023-12-06 00:23:30 +0800

    feat: partial implementation of part 2

--- 2023/day4/src/main.rs
+++ 2023/day4/src/main.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, borrow::BorrowMut};
+
 const INPUT_FILE: &str = "testcase.txt";
 
 fn main() {
@@ -9,20 +11,28 @@ fn main() {
         val.trim()
     }).collect();
     let mut points = 0;
+    let mut card_map: HashMap<&usize,u64> = std::collections::HashMap::new();
+    let mut curr_card_no = std::borrow::Cow::Borrowed(&0);
     for idx in 0..lines.len() {
         let line = lines.get(idx).unwrap();
         let nos = line.split("|").collect::<Vec<&str>>();
         let winning_no = parse_line(nos[0].split(":").collect::<Vec<&str>>()[1]);
         let my_no = parse_line(nos[1]);
         let intersections = winning_no.intersection(&my_no);
-        let no_intersections: u32 = intersections.count().try_into().unwrap();
+        let no_intersections: u64 = intersections.count().try_into().unwrap();
         if no_intersections == 0 {
             continue;
         }
         // Part 1.
-        points += i32::pow(2, no_intersections - 1);
+        points += i32::pow(2, (no_intersections - 1).try_into().unwrap());
 
-
+        // Part 2.
+        curr_card_no = std::borrow::Cow::Owned(idx + 1);
+        if card_map.get(&curr_card_no.clone().to_be()).unwrap() == &0{
+            card_map.insert(&curr_card_no, no_intersections);
+        } else {
+            card_map.insert(&curr_card_no, card_map.get(&curr_card_no.clone().to_be()).unwrap() * no_intersections);
+        }
     }
     println!("Part 1 points: {}", points);
 }
