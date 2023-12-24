c5aea90ee1b0e622ead994dc9cb696625fb6e9d1
Wen Junhua <wenjh1998@gmail.com>
2023-12-25 00:35:40 +0900

    feat: partial day 7

---
+++ 2023/day7/main.py
@@ -0,0 +1,75 @@
+from typing import List, Dict
+from types import LambdaType
+
+FILE_NAME: str = "input.txt"
+RANKING: List[str] = "AKQJT98765432"
+RANKING: List[str] = RANKING[::-1]
+HIERARCHY_MAP: Dict[int, LambdaType] = {
+    5: lambda _: (18,),
+    4: lambda _: (17,),
+    3: lambda x: (16,) if 2 in set(x.values()) else (15,),
+    2: lambda _: (14,),
+    1: lambda x: tuple(map(lambda y: RANKING.index(y), x.keys())),
+}
+
+
+class Bet:
+    def __init__(self: "Bet", cards: str, bet: int) -> None:
+        self.cards = cards
+        self.bet = bet
+        self.strength = self.calc_strength(self.cards)
+
+    def calc_strength(self: "Bet", cards: str) -> int:
+        freq = {}
+        for card in cards:
+            freq[card] = freq.get(card, 0) + 1
+        return HIERARCHY_MAP.get(max(freq.values()))(freq)
+
+    def to_alt_rep(self: "Bet") -> str:
+        return (
+            self.cards.replace("A", "E")
+            .replace("T", "A")
+            .replace("J", "B")
+            .replace("Q", "C")
+            .replace("K", "D")
+        )
+
+    def __lt__(self: "Bet", __value: "Bet") -> bool:
+        if self.strength == __value.strength:
+            return self.to_alt_rep() < __value.to_alt_rep()
+        return self.strength < __value.strength
+
+    def __eq__(self, __value: "Bet") -> bool:
+        return (
+            self.strength == __value.strength
+            and self.to_alt_rep() == __value.to_alt_rep()
+        )
+
+    def __str__(self: "Bet") -> str:
+        return self.__repr__()
+
+    def __repr__(self) -> str:
+        return f"{self.cards} ({self.strength}): {self.bet}"
+
+
+def part1(data_str: str) -> int:
+    hands = data_str.split("\n")
+    vals: List[Bet] = []
+    for hand in hands:
+        cards, value = hand.split(" ")
+        bet = Bet(cards, int(value))
+        vals.append(bet)
+    vals.sort()
+    score = 0
+    for idx, bet in enumerate(vals):
+        curr_bet = (idx + 1) * bet.bet
+        score += curr_bet
+
+    return score
+
+
+if __name__ == "__main__":
+    with open(FILE_NAME) as f:
+        data_str = f.read()
+    res = part1(data_str)
+    print("Part 1:", res)
