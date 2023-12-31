e0eccc9e2831395eb3d3c8874ee10457bc921296
Wen Junhua <wenjh1998@gmail.com>
2023-12-31 11:59:56 +0800

    feat: add part 2

--- 2023/day9/main.py
+++ 2023/day9/main.py
@@ -1,13 +1,13 @@
 from typing import List,Tuple
 
-FILE_NAME = "testcase.txt"
+FILE_NAME = "input.txt"
 
 
-def process(sequences: List[List[int]], process_fn) -> int:
+def process(sequences: List[List[int]], process_fn, res_idx: int) -> int:
     acc = 0
     for sequence in sequences:
         result = process_fn(sequence)
-        acc += result[-1]
+        acc += result[res_idx]
     return acc
 
 
@@ -25,10 +25,25 @@ def process_part1(sequence: List[int]) -> List[int]:
     return seq2
 
 
+def process_part2(sequence: List[int]) -> List[int]:
+    if len(set(sequence)) == 1:
+        sequence.append(sequence[0])
+        return sequence
+
+    seq2 = []
+    for i in range(len(sequence)-1):
+        seq2.append(sequence[i+1] - sequence[i])
+
+    result = process_part2(seq2)
+    return [sequence[0] - result[0]] +  sequence
+
+
 if __name__ == "__main__":
     with open(FILE_NAME) as f:
         data = f.readlines()
 
     processed_data = list(map(lambda x: list(map(int, x.split(" "))), data))
-    res = process(processed_data, process_part1)
+    res = process(processed_data, process_part1, -1)
     print("Part 1:", res)
+    res2 = process(processed_data, process_part2, 0)
+    print("Part 2:", res2)
