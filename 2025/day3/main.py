e38369d79d67626a9dba515d3cb0de40633d73e4
Wen Junhua <wenjh1998@gmail.com>
2025-12-03 20:26:48 +0800

    feat: add aoc for day 3 part 2

--- 2025/day3/main.py
+++ 2025/day3/main.py
@@ -21,7 +21,24 @@ def part1(data: List[List[int]]) -> int:
     return total
 
 def part2(data: List[List[int]]) -> int:
-    ...
+    total = 0
+    for row in data:
+        total += check_12_digit_max(row)
+    return total
+
+def check_12_digit_max(row: List[int]) -> int:
+    dp = [0 for _ in range(12)]
+    for idx, no in enumerate(row):
+        for offset in range(min(idx, 11), -1, -1):
+            if offset == 0:
+                dp[0] = max(dp[0], no)
+                continue
+            if dp[offset-1] == 0:
+                continue
+            dp[offset] = max(dp[offset], dp[offset-1]*10 + no)
+    return dp[-1]
+        
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Day 3 of AoC")
@@ -32,3 +49,4 @@ if __name__ == '__main__':
 
     data = read_file(filename)
     print("Part 1:", part1(data))
+    print("Part 2:", part2(data))
